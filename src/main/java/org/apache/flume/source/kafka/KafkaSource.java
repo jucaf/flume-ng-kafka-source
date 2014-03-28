3d8b39a0d836195cddb6a3a56ab5a1fbd7258d58
jucaf <jucaf1@gmail.com>
2014-03-28 10:26:28 +0100

    Update kafka version to 0.8.0

--- src/main/java/org/apache/flume/source/kafka/KafkaSource.java
+++ src/main/java/org/apache/flume/source/kafka/KafkaSource.java
@@ -19,7 +19,6 @@
 package org.apache.flume.source.kafka;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,7 +27,6 @@ import java.util.Map;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
-import kafka.message.Message;
 
 import org.apache.flume.Context;
 import org.apache.flume.Event;
@@ -52,32 +50,27 @@ import org.slf4j.LoggerFactory;
 public class KafkaSource extends AbstractSource implements Configurable, PollableSource {
 	private static final Logger log = LoggerFactory.getLogger(KafkaSource.class);
 	private ConsumerConnector consumer;
-	private ConsumerIterator<Message> it;
+	private ConsumerIterator<byte[],byte[]> it;
 	private String topic;
 	
 	public Status process() throws EventDeliveryException {
 		List<Event> eventList = new ArrayList<Event>();
-		Message message;
+		byte[] message;
 		Event event;
-		ByteBuffer buffer;
 		Map<String, String> headers;
-		byte [] bytes;
 		try {
-			if(it.hasNext()) {
-				message = it.next().message();
-				event = new SimpleEvent();
-				buffer = message.payload();
-				headers = new HashMap<String, String>();
-				headers.put("timestamp", String.valueOf(System.currentTimeMillis()));
-				bytes = new byte[buffer.remaining()];
-				buffer.get(bytes);
-				log.debug("Message: {}", new String(bytes));
-				event.setBody(bytes);
-				event.setHeaders(headers);
-				eventList.add(event);
-			}
-			getChannelProcessor().processEventBatch(eventList);
-			return Status.READY;
+                if(it.hasNext()) {
+                    message = it.next().message();
+                    event = new SimpleEvent();
+                    headers = new HashMap<String, String>();
+                    headers.put("timestamp", String.valueOf(System.currentTimeMillis()));
+                    log.debug("Message: {}", new String(message));
+                    event.setBody(message);
+                    event.setHeaders(headers);
+                    eventList.add(event);
+                }
+            getChannelProcessor().processEventBatch(eventList);
+            return Status.READY;
 		} catch (Exception e) {
 			log.error("KafkaSource EXCEPTION, {}", e.getMessage());
 			return Status.BACKOFF;
@@ -98,15 +91,11 @@ public class KafkaSource extends AbstractSource implements Configurable, Pollabl
 		}
 		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
 		topicCountMap.put(topic, new Integer(1));
-		Map<String, List<KafkaStream<Message>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+		Map<String, List<KafkaStream<byte[],byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
 		if(consumerMap == null) {
 			throw new ConfigurationException("topicCountMap is null");
 		}
-		List<KafkaStream<Message>> topicList = consumerMap.get(topic);
-		if(topicList == null || topicList.isEmpty()) {
-			throw new ConfigurationException("topicList is null or empty");
-		}
-	    KafkaStream<Message> stream =  topicList.get(0);
+	    KafkaStream<byte[],byte[]> stream = consumerMap.get(topic).get(0);
 	    it = stream.iterator();
 	}
 
